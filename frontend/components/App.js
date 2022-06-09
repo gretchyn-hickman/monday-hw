1b3db3206112bcfbf70b52796c62638ee8669e2f
AnneAugust606 <82916149+gretchyn-hickman@users.noreply.github.com>
2022-06-09 17:14:50 -0600

    added state

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,17 +1,49 @@
 import React from "react";
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: "Freshies Stickers",
+          id: 1234,
+          done: false,
+        },
+        {
+          name: "Finish Homework",
+          id: 1235,
+          done: false,
+        },
+        {
+          name: "Do Laundery",
+          id: 1236,
+          done: true,
+        },
+        {
+          name: "Journal Entry",
+          id: 1237,
+          done: false,
+        },
+      ],
+    };
+  }
   render() {
+    const { todos } = this.state;
+    console.log(todos);
     return (
       <div>
         <h1>Todo</h1>
-
         <ul>
-          <li>Monday HW</li>
-          <li>Tuesday HW</li>
-          <li>Wednesday HW</li>
-          <li>Thursday HW</li>
+          {todos.map((todo) => {
+            return (
+              <li key={todo.id}>
+                {todo.name} {todo.done ? <span>- done</span> : <span></span>}
+              </li>
+            );
+          })}
         </ul>
+
         <form>
           <input />
           <button>Add Task</button>
