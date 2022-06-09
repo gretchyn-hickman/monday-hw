033dcbbe57d146993914706dd3c086c3a0c5fab3
AnneAugust606 <82916149+gretchyn-hickman@users.noreply.github.com>
2022-06-09 16:45:57 -0600

    it begins

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
 
 export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>Todo</h1>
+
+        <ul>
+          <li>Monday HW</li>
+          <li>Tuesday HW</li>
+          <li>Wednesday HW</li>
+          <li>Thursday HW</li>
+        </ul>
+        <form>
+          <input />
+          <button>Add Task</button>
+        </form>
+        <button>Clear</button>
       </div>
-    )
+    );
   }
 }
